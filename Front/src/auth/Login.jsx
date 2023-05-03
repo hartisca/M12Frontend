f47ce4f07c2fab6b27f7e0373cc0c5311ed82b06
hector-artiso <hearca@fp.insjoaquimmir.cat>
2023-05-03 19:16:28 +0200

    Acabats login i register, falta fer fetch

--- Front/src/auth/Login.jsx
+++ Front/src/auth/Login.jsx
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react'
-import { BsMailbox } from 'react-icons/bs'
-import { RiLockPasswordLine } from 'react-icons/ri'
+import React, { useContext, useState } from 'react'
+import {FaRegEye, FaRegEyeSlash} from 'react-icons/fa';
+
+
 import {
     MDBBtn,
     MDBContainer,
@@ -15,6 +16,12 @@ import {
 
 
 export default function Login({setRegister}) {  
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
   
 
   return (
@@ -29,17 +36,18 @@ export default function Login({setRegister}) {
                 <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
                 <p className="text-white-50 mb-5">Entra el teu email i contrasenya!</p>
 
-                <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' placeholder='Email' id='formControlLg' type='email' size="lg"/>
-                <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' placeholder='Contrassenya' id='formControlLg' type='password' size="lg"/>
-
-                <p className="small mb-3 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
-                <button outline className='btnlogin mx-2 mb-5 px-5' color='white' size='lg'>
-                Login
+                <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' placeholder='Email' type='email' size="lg"/>              
+                <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' placeholder='Contrassenya' type={showPassword ? 'text' : 'password'} size="lg"/>
+                <button type="button" onClick={handlePasswordToggle} className="btn-toggle-password">
+                  {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
                 </button>
 
+                <p className="small mb-3 pb-lg-2"><a className="text-white-50" href="#!">Forgot password? </a></p>
+                <button className='btnlogin mx-2 mb-5 px-5' color='white' size='lg'> Login </button>
+
                 <div>
-                    <p className="mb-0">No tens compte? <a onClick={()=>{setRegister(false);}} class="text-white-50 fw-bold">Registra't</a></p>
-                </div>
+                  <p className="regis mb-0">No tens compte? <a onClick={()=>{setRegister(false);}} className="text-white-50 fw-bold">Registra't</a></p>
+                </div> 
             </MDBCardBody>
             </MDBCard>
 
--- Front/src/auth/Register.jsx
+++ Front/src/auth/Register.jsx
@@ -1,16 +1,66 @@
-import React from 'react'
-import {BsFillPersonFill} from 'react-icons/bs'
+import React, { useContext, useState } from 'react'
+import {FaRegEye, FaRegEyeSlash} from 'react-icons/fa';
+import { useForm } from "react-hook-form";
+
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBIcon
+}
+from 'mdb-react-ui-kit';
 
 
-export default function Register({}){
+export default function Register({setRegister}){
+  //Estat per modificar botó de mostrar contraseña
+  const [showPassword, setShowPassword] = useState(false);
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
+
+  //useForm per gestió de formulari
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  
   return(
     <>
-      
-      
-      <a className ="registerButton"
-        onClick={() => {
-            setRegister(true);
-        }}> Already registered? </a> 
+      <MDBContainer fluid>
+        <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+        <MDBCol col='12'>
+
+            <MDBCard className='bg-dark text-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '400px'}}>
+            <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
+
+                <h2 className="fw-bold mb-2 text-uppercase">Register</h2>
+                <p className="text-white-50 mb-5">Entra les teves dades!</p>
+
+                <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' placeholder='Nom' type='text' {...register("name")} size="lg"/>
+                <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' placeholder='Email' type='email' {...register("email")} size="lg"/>
+                <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' placeholder='Contrassenya' type={showPassword ? 'text' : 'password'} {...register("password")} size="lg"/>                
+                <button type="button" onClick={handlePasswordToggle} className="btn-toggle-password">
+                  {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                </button>
+
+                
+                <button  className='btnlogin mx-2 mb-5 mt-5 px-5' onClick={handleSubmit(onSubmit)} color='white' size='lg'>
+                Registrat
+                </button>
+
+                <div>
+                    <p className="regis mb-0">Ja tens compte? <a onClick={()=>{setRegister(true);}} className="text-white-50 fw-bold">Fes Log in</a></p>
+                </div>
+            </MDBCardBody>
+            </MDBCard>
+
+        </MDBCol>
+        </MDBRow>
+
+        </MDBContainer>
     </>
   )
 }
