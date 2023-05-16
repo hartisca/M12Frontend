9a00ececf870f2ae0f9dce0ddb598e02dfecd61c
hector-artiso <hearca@fp.insjoaquimmir.cat>
2023-05-16 22:14:39 +0200

    Canvi ruta fetch

--- Front/src/Slices/Fites/thunks.js
+++ Front/src/Slices/Fites/thunks.js
@@ -8,7 +8,7 @@ export const getFites = (authToken, jugadorId, partidaId) => {
     return async (dispatch) => {
                 
         try{
-            const data = await fetch ('http://127.0.0.1:8000/api/puta/', + partida, + "/" + id, {
+            const data = await fetch ('http://127.0.0.1:8000/api/fitas/list/', + partida, + "/", + id, {
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json",
